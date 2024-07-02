15af58b5520f513a3d09423b16cf76f8c6e402b4
Dalila <dalila.fariello@gmail.com>
2024-07-02 14:08:57 +0200

    added variables & functions

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,28 +1,49 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
+//Variables
+
+let currentSlideIndex = 0;
+
+const bannerImg = document.getElementById("bannerImg");
+const tagline = document.getElementById("tagline");
+const ArrowLeft = document.querySelector(".arrow_left");
+const ArrowRight = document.querySelector(".arrow_right");
+
 //Event Listner for Arrows
 
-let baliseArrowLeft=document.querySelector(".arrow_left").addEventListener("click", () =>{
-console.log ("left arrow clicked");
+let baliseArrowLeft = document.querySelector(".arrow_left").addEventListener("click", () => {
+	console.log("left arrow clicked");
 });
 
-let baliseArrowRight=document.querySelector(".arrow_right").addEventListener("click", () =>{
-    console.log ("right arrow clicked");
-    });
+let baliseArrowRight = document.querySelector(".arrow_right").addEventListener("click", () => {
+	console.log("right arrow clicked");
+});
+
+// Functions
+
+function updateSlide(index) {
+	const slide = slides[index];
+	bannerImg.src = "./Print-it-JS-main/assets/images/slideshow/${slide.image}";
+	tagline.innerHTML = slide.tagLine;
+}
+
+function updateSlide(currentSlideIndex) {
+	currentSlideIndex = (currentSlideIndex < slides.length - 1) ? currentSlideIndex + 1 : 0;
+}
