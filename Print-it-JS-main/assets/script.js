10c529b642bc32963da7feed7d8a0b810b0102da
Dalila <dalila.fariello@gmail.com>
2024-07-05 17:16:15 +0200

    updated dynamic functions

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -21,21 +21,14 @@ const slides = [
 
 let currentSlideIndex = 0;
 
-const bannerImg = document.getElementById("bannerImg");
 const tagline = document.getElementById("tagline");
 const ArrowLeft = document.querySelector(".arrow_left");
 const ArrowRight = document.querySelector(".arrow_right");
-const dotSelected = document.querySelectorAll(".dots");
+const dotsContainer = document.querySelector(".dots");
+const bannerImg = document.getElementById("bannerImg");
+
 
 //Event Listner for Arrows
-
-/*let baliseArrowLeft = document.querySelector(".arrow_left").addEventListener("click", () => {
-	console.log("left arrow clicked");
-});
-
-let baliseArrowRight = document.querySelector(".arrow_right").addEventListener("click", () => {
-	console.log("right arrow clicked");
-});*/
 
 ArrowLeft.addEventListener("click", () => {
 	if (currentSlideIndex > 0) {
@@ -53,23 +46,46 @@ ArrowRight.addEventListener("click", () => {
 	if (currentSlideIndex < slides.length - 1) {
 		currentSlideIndex = currentSlideIndex + 1;
 	}
-	else {
+	else { 
 		currentSlideIndex = 0;
 	}
 	changeSlide();
 	console.log("right arrow clicked")
 });
 
-// Functions
+// Functions to change slide
 
-function changeSlide(index) {
-	const slide = slides[currentSlideIndex];
+function changeSlide() {
+	const slide = slides[currentSlideIndex]; 
 	bannerImg.src = "./Print-it-JS-main/assets/images/slideshow/" + slide.image;
 	tagline.innerHTML = slide.tagLine;
+	updateDots();
+}
 
+// Function to create the dots
 
-	dotSelected.forEach((dot, index) => {
-		if (index === currentSlideIndex) {
+function createDots() {
+	dotsContainer.innerHTML='';
+	slides.forEach((slide,index)=>{
+		const dot = document.createElement("div");
+		dot.classList.add("dot");
+		if (index === currentSlideIndex){
+			dot.classList.add("dot_selected");
+		}
+		dotsContainer.appendChild(dot);
+		dot.addEventListener("click",()=>{
+			currentSlideIndex = index;
+			changeSlide();
+		});
+	})
+}
+
+// Function to update the dots
+
+function updateDots(){
+	const dots = dotsContainer.querySelectorAll(".dot");
+	dots.forEach((dot,index)=>{
+		if (index === currentSlideIndex){
 			dot.classList.add("dot_selected");
 		} else {
 			dot.classList.remove("dot_selected");
@@ -77,12 +93,8 @@ function changeSlide(index) {
 	});
 }
 
-/*function updateSlide(index) {
-	const slide = slides[index];
-	bannerImg.src = "./Print-it-JS-main/assets/images/slideshow/${slide.image}";
-	tagline.innerHTML = slide.tagLine;
-}
+// Call the function
 
-function updateSlide(currentSlideIndex) {
-	currentSlideIndex = (currentSlideIndex < slides.length - 1) ? currentSlideIndex + 1 : 0;
-}*/
+changeSlide();
+createDots();
+updateDots();
